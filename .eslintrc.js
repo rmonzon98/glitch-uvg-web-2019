403fd1b766d9e27eccc141b64aaa8c4d24ba486f
Raúl Monzon <yoralale@hotmail.com>
2019-11-17 17:57:35 -0600

    Errores corregidos

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    node: true,
   },
   extends: [
     'airbnb',
@@ -21,5 +22,6 @@ module.exports = {
     'react',
   ],
   rules: {
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }]
   },
 };
--- src/DataInfo.js
+++ src/DataInfo.js
@@ -1,14 +1,11 @@
+import React from 'react';
 
-import React, { Component } from 'react';
-
-export default class DataInfo extends Component {
-  render () {
-    return (
-      <ol>
-        <li>Block advertising trackers.</li>
-        <li>Keep your search history private</li>
-        <li>Take control of your personal data.</li>
-      </ol>
-    );
-  }
+export default function DataInfo() {
+  return (
+    <ol>
+      <li>Block advertising trackers.</li>
+      <li>Keep your search history private</li>
+      <li>Take control of your personal data.</li>
+    </ol>
+  );
 }
